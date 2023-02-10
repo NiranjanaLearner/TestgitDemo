830b6fc104897287bdfffa70acb5c0ce9ff046d8
Niranjana <niranjanalearner@gmail.com>
2023-02-10 08:54:29 +0530

    Addition_1.java

---
+++ Testdemogit.java/src/com/testclassmethod/Addition_1.java
@@ -0,0 +1,14 @@
+package com.testclassmethod;
+
+public class Addition_1 {
+public void addition_2()
+{
+	int a=100;
+	int b=50;
+	int c=a+b;
+	System.out.println("Additon result" +c);
+}
+	
+	
+	
+}
---
+++ Testdemogit.java/src/com/testclassmethod/Testclass_Main.java
@@ -0,0 +1,29 @@
+package com.testclassmethod;
+
+public class Testclass_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+	System.out.println("Main Method");
+		addOperation();
+		Addition_1 obj_1=new Addition_1();
+		obj_1.addition_2();
+		
+	}
+		
+	public static void addOperation()
+	{	
+		int a=100;
+		int b=200;
+		int c=a+b;
+		System.out.println("addOperation Method");
+		System.out.println("Result of addition"+c);
+	}
+}
+				
+		
+		
+		
+	
+
